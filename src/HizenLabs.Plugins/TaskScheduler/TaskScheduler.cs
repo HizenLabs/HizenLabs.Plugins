622c5ddabc738dc28b3bbccd113f1d214c190fda
Ethan Delong <48994953+EthanDelong@users.noreply.github.com>
2026-06-22 19:31:20 -0400

    update root namespace and add taskscheduler stub

---
+++ src/HizenLabs.Plugins/TaskScheduler/TaskScheduler.cs
@@ -0,0 +1,9 @@
+﻿using Oxide.Plugins;
+
+namespace HizenLabs.Plugins.TaskScheduler;
+
+[Info("Task Scheduler", "hizen", "")]
+[Description("Task scheduling system for managing automated tasks in servers.")]
+public class TaskScheduler
+{
+}
