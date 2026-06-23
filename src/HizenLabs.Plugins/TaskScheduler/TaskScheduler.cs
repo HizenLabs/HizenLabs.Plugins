0f4ff6cac8167337a1c04ea6aae85332bc594656
Ethan Delong <48994953+EthanDelong@users.noreply.github.com>
2026-06-23 10:20:20 -0400

    add version transform

--- src/HizenLabs.Plugins/TaskScheduler/TaskScheduler.cs
+++ src/HizenLabs.Plugins/TaskScheduler/TaskScheduler.cs
@@ -2,7 +2,6 @@
 
 namespace HizenLabs.Plugins.TaskScheduler;
 
-[Info("Task Scheduler", "hizen", "")]
 [Description("Schedule and automate recurring server tasks.")]
 public class TaskScheduler : PluginBase
 {
