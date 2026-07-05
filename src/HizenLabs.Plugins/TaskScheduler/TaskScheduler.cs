02047dee4616ed77227828320f04613d3d1b8cb3
Ethan Delong <48994953+EthanDelong@users.noreply.github.com>
2026-07-05 16:39:57 -0400

    remove tasksched, update editorconfig, and update testplugin

--- src/HizenLabs.Plugins/TaskScheduler/TaskScheduler.cs
+++
@@ -1,8 +0,0 @@
-﻿using HizenLabs.Shared;
-
-namespace HizenLabs.Plugins.TaskScheduler;
-
-[Description("Schedule and automate recurring server tasks.")]
-public class TaskScheduler : PluginBase
-{
-}
